3086c4996ab0e2ff4cf1193b56c3aafa86902516
Mendes113 <andremendes0113@gmail.com>
2023-08-27 16:35:21 -0300

    feat: :sparkles: Dependencias e repositório para producer e consumer

---
+++ trafficconsumer/src/main/java/com/trafficconsumer/trafficconsumer/TrafficconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.trafficconsumer.trafficconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficconsumerApplication.class, args);
+	}
+
+}

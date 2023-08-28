53c0b0a854e81b3c0c13274de218a7ceab4e2918
Mendes113 <andremendes0113@gmail.com>
2023-08-27 22:12:53 -0300

    feat(Configuração docker-kafka-spring): :sparkles: Configuração inicial para kafka docker e spring
    Para rodar o projeto, é necessário iniciar o docker com os arquivos definidos ( zookeeper etc) para testar faça uma requisiçao no postman com uma mensagem

--- trafficproducer/src/main/java/com/trafficproducer/trafficproducer/TrafficproducerApplication.java
+++ trafficproducer/src/main/java/com/trafficproducer/trafficproducer/TrafficproducerApplication.java
@@ -2,8 +2,12 @@ package com.trafficproducer.trafficproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.trafficproducer.trafficproducer.resource.MensagemResource;
 
 @SpringBootApplication
+
 public class TrafficproducerApplication {
 
 	public static void main(String[] args) {

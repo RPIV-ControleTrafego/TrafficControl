926f4330af4a730e8a4d26ee8817802c24210b79
Júlio Silveira <julio.silveira@ayvu.net>
2023-08-27 23:10:16 -0300

    Projeto de teste/configuração inicial (dependências, criação do BD (users) e 1a visualização no localhost)

---
+++ trafego/src/main/java/com/rp/trafego/TrafegoApplication.java
@@ -0,0 +1,14 @@
+package com.rp.trafego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class TrafegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafegoApplication.class, args);
+	}
+
+}

41bd7402d60a6e44d0d0e6d51842eba31ced2066
Júlio Silveira <julio.silveira@ayvu.net>
2023-08-28 11:20:10 -0300

    Crição do model 'Admin', configuração do BD (usuário admin)

--- trafego/src/main/java/com/rp/trafego/controllers/HomeController.java
+++ trafego/src/main/java/com/rp/trafego/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package com.rp.trafego.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         return "home/index";
     }
 }
---
+++ trafego/src/main/java/com/rp/trafego/repository/AdminRepo.java
@@ -0,0 +1,9 @@
+package com.rp.trafego.repository;
+
+import com.rp.trafego.models.Admin;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminRepo extends CrudRepository<Admin, Integer>{
+    //
+}

7f4df410c6df5cd72fa66161dde12c956945b866
Júlio Silveira <julio.silveira@ayvu.net>
2023-08-28 22:23:48 -0300

    Exclusão de admins (tbm removendo do BD)

--- trafego/src/main/java/com/rp/trafego/controllers/AdminController.java
+++ trafego/src/main/java/com/rp/trafego/controllers/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.rp.trafego.repository.AdminRepo;
@@ -27,11 +28,17 @@ public class AdminController {
     @GetMapping("/administradores/newAdmin")
     public String novo() {
         return "administradores/newAdmin";
-  }
+    }
 
     @PostMapping("/administradores/criar")
     public String criar(Admin admin){
         repo.save(admin);
         return "redirect:/administradores";
-  }
+    }
+
+    @GetMapping("/administradores/{id}/excluir")
+    public String excluir(@PathVariable int id){
+        repo.deleteById(id);
+        return "redirect:/administradores";
+    }
 }

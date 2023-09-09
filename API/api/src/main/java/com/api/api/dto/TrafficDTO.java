07f6c6fa5139fee65faa4a1a102f028c34a6602a
Mendes <andremendes0113@gmail.com>
2023-09-09 19:10:04 -0300

    feat(api spring): :sparkles: Novos atributos e métodos para tratamento adicionados a traffic

--- API/api/src/main/java/com/api/api/dto/TrafficDTO.java
+++ API/api/src/main/java/com/api/api/dto/TrafficDTO.java
@@ -19,5 +19,14 @@ public class TrafficDTO {
     private String carType;
     private String carColor;
     private String carBrand;
-    
+    private String veiculeOwnerName;
+    private String veiculeOwneCPF;
+    private String time;
+    private String date;
+    private String address;
+    private double speed;
+    private int maxSpeed;
+    private String direction; // north, south, east, west
+    private String streetDirection; // north, south, east, west
+
 }

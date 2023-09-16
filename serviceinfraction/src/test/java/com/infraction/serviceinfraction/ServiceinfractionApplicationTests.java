b9fb6c55ab000dd18a644ccecb303df3504ffc76
Mendes <andremendes0113@gmail.com>
2023-09-16 19:52:18 -0300

    feat: :construction: Projeto criado, serviço consumindo mensagens do kafka e salvando no banco
    O kafka foi configurado na serviço para consumir informações de infração. Elas são consumidas e processadas pelo service que as salva no banco de dados.  Para que tudo funcionasse foi necessário criar um DTO, Entity e repository. Ainda há métodos a serem inseridos

---
+++ serviceinfraction/src/test/java/com/infraction/serviceinfraction/ServiceinfractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infraction.serviceinfraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceinfractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

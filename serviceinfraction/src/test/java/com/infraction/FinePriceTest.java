db56ad99b7239e4154222ef59fb31b3e8c094ae3
Mendes <andremendes0113@gmail.com>
2023-09-19 15:06:52 -0300

    refactor: :recycle: deletando comentários auto gerados nos testes

--- serviceinfraction/src/test/java/com/infraction/FinePriceTest.java
+++ serviceinfraction/src/test/java/com/infraction/FinePriceTest.java
@@ -7,31 +7,26 @@ import org.junit.jupiter.api.Test;
 import com.infraction.serviceinfraction.calculator.FinePriceCalculator;
 import com.infraction.serviceinfraction.dto.InfractionDTO;
 
-
-
 public class FinePriceTest {
 
-        
-     @Test
+    @Test
     void testCalculateFinePriceSpeeding1() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("speeding");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(45.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(45.0, finePrice); 
     }
 
     @Test
     void testCalculateSpeedingFine2() {
         InfractionDTO infractionDTO = new InfractionDTO();
-        infractionDTO.setSpeed(100); 
-        infractionDTO.setMaxSpeed(80); 
+        infractionDTO.setSpeed(100);
+        infractionDTO.setMaxSpeed(80);
 
         double speedingFine = FinePriceCalculator.calculateFinePrice("speeding", infractionDTO);
 
-     
-      
         double expectedFine = (100 - 80) * 45.0;
         assertEquals(expectedFine, speedingFine);
     }
@@ -39,14 +34,13 @@ public class FinePriceTest {
     @Test
     void testCalculateSpeedingAndWrongDirection() {
         InfractionDTO infractionDTO = new InfractionDTO();
-        infractionDTO.setSpeed(100); 
-        infractionDTO.setMaxSpeed(80); 
+        infractionDTO.setSpeed(100);
+        infractionDTO.setMaxSpeed(80);
         infractionDTO.setViolation("wrong direction");
 
         double expectedSpeedingFine = (100 - 80) * FinePriceCalculator.calculateFinePrice("speeding");
         double expectedWrongDirectionFine = FinePriceCalculator.calculateFinePrice("wrong direction");
 
-    
         double expectedTotalFine = expectedSpeedingFine + expectedWrongDirectionFine;
 
         double totalFine = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
@@ -57,8 +51,8 @@ public class FinePriceTest {
     @Test
     void testCalculateFinePriceNoViolation() {
         InfractionDTO infractionDTO = new InfractionDTO();
-        infractionDTO.setSpeed(100); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(100);
+        infractionDTO.setMaxSpeed(180);
 
         double speedingFine = FinePriceCalculator.calculateFinePrice("speeding", infractionDTO);
 
@@ -68,8 +62,8 @@ public class FinePriceTest {
     @Test
     void testCalculateFinePriceNoViolation2() {
         InfractionDTO infractionDTO = new InfractionDTO();
-        infractionDTO.setSpeed(100); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(100);
+        infractionDTO.setMaxSpeed(180);
         infractionDTO.setViolation("no violation");
 
         double speedingFine = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
@@ -77,12 +71,11 @@ public class FinePriceTest {
         assertEquals(0.0, speedingFine);
     }
 
-    
     @Test
     void testCalculateFinePriceNoViolation3() {
         InfractionDTO infractionDTO = new InfractionDTO();
-        infractionDTO.setSpeed(100); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(100);
+        infractionDTO.setMaxSpeed(180);
         infractionDTO.setViolation(null);
 
         double speedingFine = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
@@ -90,301 +83,237 @@ public class FinePriceTest {
         assertEquals(0.0, speedingFine);
     }
 
-    
     @Test
-    void testCalculateNoPlate(){
+    void testCalculateNoPlate() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no plate");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(300.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(300.0, finePrice); 
     }
 
-
     @Test
-    void testCalculateNoPlateAndSpeeding(){
+    void testCalculateNoPlateAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no plate");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(1200.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(1200.0, finePrice); 
     }
 
-
     @Test
-    void testCalculateRedLight(){
+    void testCalculateRedLight() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("red light");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(150.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(150.0, finePrice); 
     }
 
     @Test
-    void testCalculateRedLightAndSpeeding(){
+    void testCalculateRedLightAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("red light");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(1050.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(1050.0, finePrice); 
     }
 
     @Test
-    void testCalculateStopSign(){
+    void testCalculateStopSign() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("stop sign");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(120.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(120.0, finePrice);
     }
 
     @Test
-    void testCalculateStopSignAndSpeeding(){
+    void testCalculateStopSignAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("stop sign");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(1020.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(1020.0, finePrice); 
     }
 
     @Test
-    void testCalculateNoSeatBelt(){
+    void testCalculateNoSeatBelt() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no seat belt");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(80.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(80.0, finePrice);
     }
 
     @Test
-    void testCalculateNoSeatBeltAndSpeeding(){
+    void testCalculateNoSeatBeltAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no seat belt");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(980.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(980.0, finePrice);
     }
 
     // @Test
     // void testCalculateNoHelmet(){
-    //     InfractionDTO infractionDTO = new InfractionDTO();
-    //     infractionDTO.setViolation("no helmet");
+    // InfractionDTO infractionDTO = new InfractionDTO();
+    // infractionDTO.setViolation("no helmet");
 
-    //     double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
+    // double finePrice =
+    // FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(),
+    // infractionDTO);
 
-    //     assertEquals(90.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+    // assertEquals(90.0, finePrice); // Assuming speeding fine is 45.0 as per your
+    // code
     // }
 
     @Test
-    void testCalculateNoInsurance(){
+    void testCalculateNoInsurance() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no insurance");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(150.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(150.0, finePrice); 
     }
 
     @Test
-    void testCalculateNoInsuranceAndSpeeding(){
+    void testCalculateNoInsuranceAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no insurance");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(1050.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(1050.0, finePrice); 
     }
 
     @Test
-    void testCalculateNoRegistration(){
+    void testCalculateNoRegistration() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no registration");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(100.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(100.0, finePrice); 
     }
 
     @Test
-    void testCalculateNoRegistrationAndSpeeding(){
+    void testCalculateNoRegistrationAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("no registration");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(1000.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(1000.0, finePrice); 
     }
 
     @Test
-    void testCalculateDrunkDriving(){
+    void testCalculateDrunkDriving() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("drunk driving");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(500.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(500.0, finePrice); 
     }
 
     @Test
-    void testCalculateDrunkDrivingAndSpeeding(){
+    void testCalculateDrunkDrivingAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("drunk driving");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(1400.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(1400.0, finePrice); 
     }
 
     @Test
-    void testCalculateRecklessDriving(){
+    void testCalculateRecklessDriving() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("reckless driving");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(400.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(400.0, finePrice); 
     }
 
     @Test
-    void testCalculateRecklessDrivingAndSpeeding(){
+    void testCalculateRecklessDrivingAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("reckless driving");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(1300.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(1300.0, finePrice); 
     }
 
     @Test
-    void testCalculateHitAndRun(){
+    void testCalculateHitAndRun() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("hit and run");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(600.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(600.0, finePrice); 
     }
 
     @Test
-    void testCalculateHitAndRunAndSpeeding(){
+    void testCalculateHitAndRunAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("hit and run");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
-        
-        assertEquals(1500.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+
+        assertEquals(1500.0, finePrice); 
     }
 
     @Test
-    void testCalculateTailgating(){
+    void testCalculateTailgating() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("tailgating");
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
 
-        assertEquals(100.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+        assertEquals(100.0, finePrice); 
     }
 
     @Test
-    void testCalculateTailgatingAndSpeeding(){
+    void testCalculateTailgatingAndSpeeding() {
         InfractionDTO infractionDTO = new InfractionDTO();
         infractionDTO.setViolation("tailgating");
-        infractionDTO.setSpeed(200); 
-        infractionDTO.setMaxSpeed(180); 
+        infractionDTO.setSpeed(200);
+        infractionDTO.setMaxSpeed(180);
 
         double finePrice = FinePriceCalculator.calculateFinePrice(infractionDTO.getViolation(), infractionDTO);
-        
-        assertEquals(1000.0, finePrice); // Assuming speeding fine is 45.0 as per your code
+
+        assertEquals(1000.0, finePrice); 
     }
-
-
-
-
-
 
 }
-
-
-// package com.infraction.serviceinfraction.calculator;
-
-// import java.util.HashMap;
-// import java.util.Map;
-
-// import com.infraction.serviceinfraction.dto.InfractionDTO;
-
-// public class FinePriceCalculator {
-//     private static final Map<String, Double> finePrices = new HashMap<>();
-
-//     static {
-//         finePrices.put("red light", 150.0);
-//         finePrices.put("stop sign", 120.0);
-//         finePrices.put("no seat belt", 80.0);
-//         finePrices.put("no helmet", 90.0);
-//         finePrices.put("no license", 200.0);
-//         finePrices.put("no insurance", 150.0);
-//         finePrices.put("no registration", 100.0);
-//         finePrices.put("drunk driving", 500.0);
-//         finePrices.put("reckless driving", 400.0);
-//         finePrices.put("hit and run", 600.0);
-//         finePrices.put("tailgating", 100.0);
-//         finePrices.put("jaywalking", 50.0);
-//         finePrices.put("illegal turn", 80.0);
-//         finePrices.put("parking violation", 100.0);
-//         finePrices.put("speeding", 45.0);
-//         finePrices.put("wrong direction", 25.0);
-//         finePrices.put("no plate", 300.0);
-//     }
-
-//     public static double calculateFinePrice(String violation, InfractionDTO infractionDTO) {
-//         if (infractionDTO == null) {
-//             throw new IllegalArgumentException("InfractionDTO não pode ser nulo.");
-//         }
-
-//         double finePrice = 0;
-
-//         if (infractionDTO.getSpeed() > infractionDTO.getMaxSpeed()) {
-//             finePrice = calculateSpeedingFine(infractionDTO);
-//         }
-
-//         if (infractionDTO.getViolation() != null) {
-//             finePrice += calculateFinePrice(infractionDTO.getViolation().toLowerCase());
-//         }
-
-//         return finePrice;
-//     }
-
-//     private static double calculateSpeedingFine(InfractionDTO infractionDTO) {
-//         return Math.max(0, infractionDTO.getSpeed() - infractionDTO.getMaxSpeed()) * finePrices.get("speeding");
-//     }
-
-//     public static double calculateFinePrice(String violation) {
-//         return finePrices.getOrDefault(violation, 0.0);
-//     }
-
-// }

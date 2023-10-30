6d94e27f5492d983ee0a8e5953b9d2b20b0b9996
Mendes <andremendes0113@gmail.com>
2023-10-29 22:37:08 -0300

    fix manual

--- traffic/src/main/java/com/traffic/traffic/message/KafkaConsumerMessage.java
+++ traffic/src/main/java/com/traffic/traffic/message/KafkaConsumerMessage.java
@@ -1,17 +1,13 @@
 package com.traffic.traffic.message;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
 
 
 import org.springframework.kafka.annotation.KafkaListener;
 
 import com.traffic.traffic.Service.TrafficService;
+import com.traffic.traffic.dto.AllTraficDTO;
 import com.traffic.traffic.dto.InfractionDTO;
 import com.traffic.traffic.dto.TrafficDto;
+import com.traffic.traffic.dto.Mapper.TrafficMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,17 +24,12 @@ public class KafkaConsumerMessage {
     @Autowired
     private TrafficService trafficService;
 
-    /**
-     * Listens to the "traffic-topic" Kafka topic with a group ID of "traffic-topic".
-     * Receives traffic information in the form of a TrafficDto object.
-     * Logs the received traffic information using a logger.
-     * Calls the newCarDetails() method in the TrafficService class to process the received traffic details.
-     *
-     * @param trafficInfo the traffic information received
-     */
+    
     @KafkaListener(topics = "traffic-topic", groupId = "traffic-topic")
-    public void listening(TrafficDto trafficInfo) {
+    public void listening(AllTraficDTO trafficInfo) {
         LOG.info("Traffic service - Received traffic info: {}", trafficInfo);
+
+    
         trafficService.newCarDetails(trafficInfo);
     }
 }

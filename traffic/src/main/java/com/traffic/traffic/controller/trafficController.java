281bc57790b6e3ea2a05471527139885d95f60cc
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-07 19:12:07 -0300

    refactor: :art: Remoçao dos imports não utilizados
    Removi os imports

--- traffic/src/main/java/com/traffic/traffic/controller/trafficController.java
+++ traffic/src/main/java/com/traffic/traffic/controller/trafficController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +12,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.traffic.traffic.Service.ITrafficService;
 import com.traffic.traffic.dto.TrafficDto;
-import com.traffic.traffic.repository.TrafficRepository;
-
-import ch.qos.logback.core.model.Model;
 
 @Controller
 @RequestMapping("/service/traffic")

25efae7c145a87f908f13de31f3bb96d0fe6f7a3
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-26 20:20:09 -0300

    feat: :sparkles: att service
    Melhoria do codigo de serviços acidentes

--- serviceaccident/src/main/java/com/accident/serviceaccident/service/AccidentService.java
+++ serviceaccident/src/main/java/com/accident/serviceaccident/service/AccidentService.java
@@ -1,71 +1,59 @@
 package com.accident.serviceaccident.service;
 
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
-
 import com.accident.serviceaccident.Entity.AccidentEntity;
-// import com.accident.serviceaccident.client.AccidentClient;
 import com.accident.serviceaccident.dto.AccidentDTO;
 import com.accident.serviceaccident.logger.AccidentLogger;
 import com.accident.serviceaccident.repository.AccidentRepository;
 
-
-
 @Service
-public class AccidentService implements IAccidentService{
-    
+public class AccidentService implements IAccidentService {
+
     private final AccidentLogger logger = new AccidentLogger(AccidentService.class);
 
+    private final AccidentRepository accidentRepository;
 
     @Autowired
-    private AccidentRepository accidentRepository;
-
+    public AccidentService(AccidentRepository accidentRepository) {
+        this.accidentRepository = accidentRepository;
+    }
 
     public void newAccidentDetails(AccidentDTO accidentDTO) {
-        logger.info("Accident service - Received accident information: {}", accidentDTO);
+        if (accidentDTO == null) {
+            logger.error("Received null AccidentDTO. Cannot save accident information.");
+            return;
+        }
+
+        if (!isAccidentValid(accidentDTO)) {
+            logger.error("Invalid AccidentDTO received. Not saving accident information: {}");
+            return;
+        }
 
         try {
-            
             AccidentEntity accidentEntity = mapAccidentDTOToEntity(accidentDTO);
-           
             accidentRepository.save(accidentEntity);
-            logger.info("Accident service - Accident information saved successfully: {}", accidentEntity);
-        } catch (Exception e) {
-            logger.error("Accident service - Error while saving accident information: {}", e.getMessage());
+            logger.info("Accident information saved successfully: {}", accidentEntity);
+        } catch (DataAccessException e) {
+            logger.error("Error while saving accident information: {}", e.getMessage());
+            // Handle the exception as needed
         }
     }
 
-
-    private AccidentEntity mapAccidentDTOToEntity(AccidentDTO accidentDTO){
-        
-        if(!isAccidentValid(accidentDTO)) {
-            return null;
-        }
-    
+    private AccidentEntity mapAccidentDTOToEntity(AccidentDTO accidentDTO) {
         AccidentEntity accidentEntity = new AccidentEntity();
         accidentEntity.setTipo(accidentDTO.getTipo());
         accidentEntity.setSeveridade(accidentDTO.getSeveridade());
         accidentEntity.setData(accidentDTO.getData());
         accidentEntity.setVitimas(accidentDTO.getVitimas());
         accidentEntity.setHora(accidentDTO.getHora());
-        
         return accidentEntity;
     }
 
     private boolean isAccidentValid(AccidentDTO accidentDTO) {
-        if(accidentDTO == null) {
-            logger.info("AccidentDTO is null, not saving accident information");
-            return false;
-        }
-       
-        if (accidentDTO.getTipo() == null || accidentDTO.getSeveridade() == 0 || accidentDTO.getData() == null) {
-            logger.info("AccidentDTO is invalid, not saving accident information");
-            return false;
-        }
-        return true;
+        return accidentDTO.getTipo() != null
+                && accidentDTO.getSeveridade() > 0
+                && accidentDTO.getData() != null;
     }
-
 }

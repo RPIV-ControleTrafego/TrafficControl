9bbe5f0182003410de15729a449dbd5e769b455b
ARTHUR9011 <72285627+ARTHUR9011@users.noreply.github.com>
2023-11-26 20:46:47 -0300

    fix: :sparkles: melhoria e att
    melgoria no producer e att

--- traffic/src/main/java/com/traffic/traffic/message/KafkaProducerConfig.java
+++ traffic/src/main/java/com/traffic/traffic/message/KafkaProducerConfig.java
@@ -1,7 +1,5 @@
 package com.traffic.traffic.message;
 
-
-import org.apache.catalina.authenticator.SpnegoAuthenticator.AcceptAction;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,80 +9,46 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.springframework.stereotype.Component;
-
-import com.traffic.traffic.dto.AccidentDTO;
-import com.traffic.traffic.dto.InfractionDTO;
-import com.traffic.traffic.dto.TrafficDto;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
-
-@Component
 @Configuration
 public class KafkaProducerConfig {
-    
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServer;
 
-    @Bean
-    public ProducerFactory<String, TrafficDto> trafficProducerFactory(){
-
-        Map<String,Object> configProps = new HashMap<>();
-
+    private Map<String, Object> producerCommonConfig() {
+        Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        return configProps;
+    }
 
-
+    private <T> ProducerFactory<String, T> createProducerFactory(Class<T> valueType) {
+        Map<String, Object> configProps = producerCommonConfig();
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
-   
-  
-
-     @Bean
-    public ProducerFactory<String, InfractionDTO> infractionProducerFactory(){
-
-        Map<String,Object> configProps = new HashMap<>();
-
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
-
-
-        return new DefaultKafkaProducerFactory<>(configProps);
+    private <T> KafkaTemplate<String, T> createKafkaTemplate(Class<T> valueType) {
+        return new KafkaTemplate<>(createProducerFactory(valueType));
     }
 
- 
-
     @Bean
-    public KafkaTemplate<String, InfractionDTO> infractionKafkaTemplate(){
-        return new KafkaTemplate<>(infractionProducerFactory());
+    public KafkaTemplate<String, TrafficDto> trafficKafkaTemplate() {
+        return createKafkaTemplate(TrafficDto.class);
     }
 
-
     @Bean
-    public ProducerFactory<String, AccidentDTO> accidentProducerFactory(){
-
-        Map<String,Object> configProps = new HashMap<>();
-
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
-        return new DefaultKafkaProducerFactory<>(configProps);
-}
-
-
-    @Bean
-    public KafkaTemplate<String, AccidentDTO> accidentKafkaTemplate(){
-        return new KafkaTemplate<>(accidentProducerFactory());
+    public KafkaTemplate<String, InfractionDTO> infractionKafkaTemplate() {
+        return createKafkaTemplate(InfractionDTO.class);
     }
-    
 
+    @Bean
+    public KafkaTemplate<String, AccidentDTO> accidentKafkaTemplate() {
+        return createKafkaTemplate(AccidentDTO.class);
+    }
 }
